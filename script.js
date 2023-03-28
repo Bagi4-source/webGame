30c3beb456b41350dfa816d7a1676d1060a79e6b
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-03-28 10:05:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,121 +2,121 @@ const questions = {
     "Как называется чертеж земной поверхности?": {
         "true_answer": 0,
         "answers": ["Карта", "Схема", "Условные знаки"],
-        "score": 10,
+        "score": 5,
         "img": "https://u.foxford.ngcdn.ru/uploads/tinymce_file/file/14205/e0a803ccccb8dc18.png"
     },
     "Выберите территорию, реки которой наиболее подходят для строительства ГЭС": {
         "true_answer": 0,
         "answers": ["Юг Восточной Сибири", "Уральские горы", "Западно-Сибирская равнина"],
-        "score": 10,
+        "score": 5,
         "img": "https://s9.travelask.ru/system/images/files/001/502/969/wysiwyg_jpg/FCF11D52-187B-42D6-A5A2-25C680C589FD.jpg?1637931559"
     },
     "Что является причиной возникновения цунами?": {
         "true_answer": 0,
         "answers": ["Моретрясение", "Извержение вулканов", "Океанский ветер"],
-        "score": 10,
+        "score": 5,
         "img": "https://waterservice.kz/blog/img/art110-1.jpg"
     },
      "Крупнейшим хранилищем запасов пресных вод в России": {
         "true_answer": 0,
         "answers": ["Озера, пруды и водохранилища", "Моря", "Реки"],
-        "score": 10,
+        "score": 5,
         "img": "https://pix-feed.com/wp-content/uploads/2018/11/1-33.jpg"
     },
     "Выберите вариант, где неверно указаны причинно-следственные связи": {
         "true_answer": 1,
         "answers": ["Создание полезащитных лесополос — уменьшает почвенную эрозию", "Орошение почв — предотвращает засоление земель", "Снегозадержание — увеличивает содержание влаги в почве"],
-        "score": 10,
+        "score": 5,
         "img": "https://gruntovozov.ru/wp-content/uploads/Zasolennyie-pochvyi/Zasolennyie-pochvyi_(34).jpg"
     },
     "Кто является первым создателем географической карты?": {
         "true_answer": 0,
         "answers": ["Эратосфен", "Аренст", "Героклит"],
-        "score": 10,
+        "score": 5,
         "img": "https://www.pnp.ru/upload/entities/2021/09/28/15/article/detailPicture/34/6a/ba/b6/3dddea75d60e01ebfea83b88eac9df64.jpg"
     },
     "Какие течения относятся к холодным?": {
         "true_answer": 2,
         "answers": ["Аляскинское и Гольфстрим", "Гвианское и Калифорнийское", "Калифорнийское и Перуанское"],
-        "score": 10,
+        "score": 5,
         "img": "https://assets-global.website-files.com/599873abab717100012c91ea/5fa147823ed7903feb284a5d_%D1%82%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D1%8F_2%20(1)%20(1)%20(1)%20(1)%20(1)%20(1).png"
     },
     "Что является причиной возникновения цунами?": {
         "true_answer": 0,
         "answers": ["Моретрясение", "Извержение вулканов", "Океанский ветер"],
-        "score": 10,
+        "score": 5,
         "img": "https://waterservice.kz/blog/img/art110-1.jpg"
     },
     "Укажите пограничное озеро России": {
         "true_answer": 2,
         "answers": ["Байкал", "Чаны", "Каспийское"],
-        "score": 10,
+        "score": 5,
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Caspian_Sea_from_orbit-NoRedLines.jpg/280px-Caspian_Sea_from_orbit-NoRedLines.jpg"
     },
     "Сколько составляет площадь поверхности всей Земли?": {
         "true_answer": 0,
         "answers": ["510.2 млн. кв. км.", "423.2 млн. кв.км.", "20.3 млн. кв. км."],
-        "score": 10,
+        "score": 5,
         "img": "https://upload.wikimedia.org/wikipedia/commons/0/0d/Africa_and_Europe_from_a_Million_Miles_Away.png"
     },
      "Какова в России средняя продолжительность жизни мужчин? (по оценке ВОЗ 2019 года)": {
         "true_answer": 2,
         "answers": ["62.1", "57.9", "60.7"],
-        "score": 10,
+        "score": 5,
         "img": "https://visasam.ru/wp-content/uploads/2017/10/5410-6-eksperty_vsemirnogo_ban_ru-450x333.jpg"
     },
     "Какова в России средняя продолжительность жизни женщин? (по оценке ВОЗ 2019 года)": {
         "true_answer": 1,
         "answers": ["59.8", "67.5", "62.4"],
-        "score": 10,
+        "score": 5,
         "img": "https://www.renlife.ru/upload/medialibrary/18a/middle-age-white-hair-woman-holding-a-sandglass-timer_1194-334309.jpg"
     },
     "Какое место в мире по численности населения занимает Россия?": {
         "true_answer": 0,
         "answers": ["8", "18", "10"],
-        "score": 10,
+        "score": 5,
         "img": "https://www.oka.fm/afisha/4563546745745676613.png"
     },
     "С какой страной проходит самая короткая граница россии?": {
         "true_answer": 1,
         "answers": ["С Эстонией", "С КНДР", "С Южной Осетией"],
-        "score": 10,
+        "score": 5,
         "img": "https://s9.travelask.ru/system/images/files/001/190/875/wysiwyg_jpg/f005.jpg?1536941628"
     },
     "Мыс Флигели расположен на острове?": {
         "true_answer": 0,
         "answers": ["Рудольфа", "Врангеля", "Ратманова"],
-        "score": 10,
+        "score": 5,
         "img": "https://img.geliophoto.com/fligely/00_fligely.jpg"
     },
-    "Нна сколько часовых поясов поделена Россия?": {
+    "На сколько часовых поясов поделена Россия?": {
         "true_answer": 2,
         "answers": ["10", "9", "11"],
-        "score": 10,
+        "score": 5,
         "img": "https://reader.lecta.rosuchebnik.ru/demo/8030/data/images/autogen_41732.jpg"
     },
     "Самая высокая точка России?": {
         "true_answer": 0,
         "answers": ["Гора Эльбрус", "Гора Казбек", "Гора Белуха"],
-        "score": 10,
+        "score": 5,
         "img": "https://www.erv.ru/upload/iblock/ca5/1.jpg"
     },
     "Какая эра является самой древней?": {
         "true_answer": 1,
         "answers": ["Архейская", "Палеозойская", "Мезозойская"],
-        "score": 10,
+        "score": 5,
         "img": "https://ykl-res.azureedge.net/dae41c9d-e624-4b77-9a5a-f5c98330aa8c/shutterstock759826384w400.png"
     },
     "Кто был автором теории литосферных плит?": {
         "true_answer": 1,
         "answers": ["М. Фернан", "А. Вегенер", "Д. Кук"],
-        "score": 10,
+        "score": 5,
         "img": "https://u.foxford.ngcdn.ru/uploads/tinymce_image/image/75975/35dc9fb8e97db489.png"
     },
      "Сколько морей омывает территорию России?": {
         "true_answer": 0,
         "answers": ["15", "18", "12"],
-        "score": 10,
+        "score": 5,
         "img": "https://s9.travelask.ru/system/images/files/001/190/875/wysiwyg_jpg/f005.jpg?1536941628"
     }
 }
